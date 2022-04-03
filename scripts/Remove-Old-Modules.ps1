00d9da78ba8af849107eff151d4365c844c57da1
Jason Lautzenheiser <lautzenheiser.jason@gmail.com>
2022-04-03 07:37:06 -0400

    Minor text change

--- scripts/Remove-Old-Modules.ps1
+++ scripts/Remove-Old-Modules.ps1
@@ -1,8 +1,8 @@
-write-host "Remove old versions of installed modules"
+write-host "Remove old versions of installed modules."
 
 $mods = Get-InstalledModule
 
 foreach ($module in $mods) {
-    Write-Verbose -Message "Uninstalling old versions of $($module.Name) [latest is $( $module.Version)]" -Verbose
+    Write-Host -Message "Removing old versions of $($module.Name) [latest is $( $module.Version)]"
     Get-InstalledModule -Name $module.name -AllVersions | Where-Object {$_.version -ne $module.version } | Uninstall-Module -Verbose
 }
