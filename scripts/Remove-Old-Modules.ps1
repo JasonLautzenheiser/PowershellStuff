702acb2ff5ed66f952ef34b4a2b032127da1434c
Jason Lautzenheiser <lautzenheiser.jason@gmail.com>
2022-04-15 10:29:20 -0400

    Updated remove-old-modules with params
    added a module-name param to specify a single module to remove
    added an -includeAll param that will grab all modules like the default is in older versions.
    no params will now give message to included a module
    -help param gives quick help
    added support for get-help

--- scripts/Remove-Old-Modules.ps1
+++ scripts/Remove-Old-Modules.ps1
@@ -1,6 +1,42 @@
-write-host "Remove old versions of installed modules."
+<# .SYNOPSIS #>
+param(
+    [Parameter()]
+    [string]$name, 
+    [Parameter()]
+    [switch]$includeAll,
+    [Parameter()]
+    [switch]$help
+)
 
-$mods = Get-InstalledModule
+if ($help) {
+    Write-Output "Usage: Remove-Old-Modules [-Name <string>] [-IncludeAll <switch>] [-Help <switch>]";
+    exit;
+}
+
+if ($includeAll.IsPresent) {
+    $mods = Get-InstalledModule 
+    if ($mods.Count -eq 0)
+    {
+        write-host "No modules found."
+        exit 1
+    }
+
+    write-host "Removing old versions of installed modules."
+
+} else {
+    if (!$name)
+    {
+        write-host "Please specify a module name."
+        exit 1
+    }
+
+    $mods = Get-InstalledModule $name 
+    if ($mods.Count -eq 0)
+    {
+        write-host "Module $name not found."
+        exit 1
+    }
+}
 
 foreach ($module in $mods) {
     Write-Host -Message "Removing old versions of $($module.Name) [latest is $( $module.Version)]"
